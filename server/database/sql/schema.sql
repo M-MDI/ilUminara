6d940447b56705f86d3c482c9bc09fb287a8a56d
mmoulabbi <Mehdi.nada909@gmail.com>
2025-02-12 22:38:40 +0100

    remove some extra cmts

--- server/database/sql/schema.sql
+++ server/database/sql/schema.sql
@@ -1,10 +1,12 @@
 PRAGMA foreign_keys = ON;
+
 CREATE TABLE IF NOT EXISTS sessions (
     user_id BIGINT UNIQUE NOT NULL,
     session_id TEXT NOT NULL,
     expires_at TIMESTAMP NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id) on DELETE CASCADE
 );
+
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     email TEXT UNIQUE NOT NULL,
@@ -12,6 +14,7 @@ CREATE TABLE IF NOT EXISTS users (
     password TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
 CREATE TABLE IF NOT EXISTS post_category (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     post_id BIGINT NOT NULL,
@@ -19,11 +22,13 @@ CREATE TABLE IF NOT EXISTS post_category (
     FOREIGN KEY (post_id) REFERENCES posts(id),
     FOREIGN KEY (category_id) REFERENCES categories(id) UNIQUE (post_id, category_id)
 );
+
 CREATE TABLE IF NOT EXISTS categories (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     label TEXT UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
 CREATE TABLE IF NOT EXISTS posts (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id BIGINT NOT NULL,
@@ -32,6 +37,7 @@ CREATE TABLE IF NOT EXISTS posts (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
+
 CREATE TABLE IF NOT EXISTS comments (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id BIGINT NOT NULL,
@@ -41,6 +47,7 @@ CREATE TABLE IF NOT EXISTS comments (
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE
 );
+
 CREATE TABLE IF NOT EXISTS post_reactions (
     user_id BIGINT NOT NULL,
     post_id BIGINT NOT NULL,
@@ -51,6 +58,7 @@ CREATE TABLE IF NOT EXISTS post_reactions (
     UNIQUE (user_id, post_id),
     CHECK (reaction IN ('like', 'dislike'))
 );
+
 CREATE TABLE IF NOT EXISTS comment_reactions (
     user_id BIGINT NOT NULL,
     comment_id BIGINT NOT NULL,
