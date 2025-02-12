86210b65545cfcbb4580436fa88f57cc6d9098ab
mmoulabbi <Mehdi.nada909@gmail.com>
2025-02-12 22:46:58 +0100

    Make this human-readable

--- server/utils/email.go
+++ server/utils/email.go
@@ -4,7 +4,11 @@ import "regexp"
 
 // IsValidEmail checks if the provided email is valid.
 func IsValidEmail(email string) bool {
+
 	const emailRegex = `^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$`
+	
 	re := regexp.MustCompile(emailRegex)
+	
 	return re.MatchString(email)
+	
 }
